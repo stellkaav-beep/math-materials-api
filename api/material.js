136abdd99715ec8b5c69e84d5e875da2f7c5fd65
stellkaav-beep <stellka.av@gmail.com>
2026-01-13 20:24:30 +0500

    Update material.js

--- api/material.js
+++ api/material.js
@@ -26,8 +26,11 @@ export default function handler(req, res) {
     const videoHwMaterials = {
       "topic_geo7_ugol_video_hw": "Видео: https://disk.yandex.ru/i/ZCKBcyqWGTGyIw\n\nДЗ: https://disk.yandex.ru/i/7lSeKDjy6ivCmA",
       "topic_alg7_stepeni_video_hw": "Видео: https://disk.yandex.ru/i/4fGagOSYfxDIQw\n\nДЗ: https://disk.yandex.ru/i/SuKynhUcUxkn8Q",
-      "topic_alg8_drobi_video_hw": "Видео: https://rutube.ru/video/private/691d52b9f3320319769fc91ac71a6b19/?p=3CxPjpZnzgzEQjfCIxIpRw\n\nДЗ: https://disk.yandex.ru/i/ioQ6ri6LIFf9IQ",
-      "topic_geo7_otrezok_video_hw": "Видео: https://disk.yandex.ru/i/oZE5Ufkg2rniRA\n\nДЗ: https://disk.yandex.ru/i/F1Wf8jkKISE_0Q"
+      "topic_geo7_otrezok_video_hw": "Видео: https://disk.yandex.ru/i/oZE5Ufkg2rniRA\n\nДЗ: https://disk.yandex.ru/i/F1Wf8jkKISE_0Q",
+      "topic_alg8_drobi_video_hw": "Видео: https://disk.yandex.ru/i/VEOxckIGqysMCQ\n\nДЗ: https://disk.yandex.ru/i/0GQVXI1uSIchkw",
+      "topic_alg8_drobiud_video_hw": "Видео: https://disk.yandex.ru/i/ORZjVM3bSB1DFw\n\nДЗ: https://disk.yandex.ru/i/viE83dNpEbXvLg",
+      "topic_geo8_mnogo_video_hw": "Видео: https://disk.yandex.ru/i/FRntWHvzRE9WEw\n\nДЗ: https://disk.yandex.ru/i/FvUpp9e1tj9XDQ",
+      "topic_geo8_mnogoprk_video_hw": "Видео: https://disk.yandex.ru/i/KP7OuS37kRtM-Q\n\nДЗ: https://disk.yandex.ru/i/6bu986kLVHw9dw"
     };
     
     const material = videoHwMaterials[key];
@@ -43,10 +46,16 @@ export default function handler(req, res) {
     "topic_geo7_ugol_video": "https://disk.yandex.ru/i/ZCKBcyqWGTGyIw",
     "topic_alg7_stepeni_text": "https://disk.yandex.ru/i/98CpuXc-4tDmGg",
     "topic_alg7_stepeni_video": "https://disk.yandex.ru/i/4fGagOSYfxDIQw",
-    "topic_alg8_drobi_text": "https://disk.yandex.ru/i/sOWc2ZxQnqZ9KQ",
-    "topic_alg8_drobi_video": "https://rutube.ru/video/private/691d52b9f3320319769fc91ac71a6b19/?p=3CxPjpZnzgzEQjfCIxIpRw",
     "topic_geo7_otrezok_text": "https://disk.yandex.ru/i/sUhuOqDxvfdINQ",
-    "topic_geo7_otrezok_video": "https://disk.yandex.ru/i/oZE5Ufkg2rniRA"
+    "topic_geo7_otrezok_video": "https://disk.yandex.ru/i/oZE5Ufkg2rniRA",
+    "topic_alg8_drobi_text": "https://disk.yandex.ru/i/DlTTrLd5_1YTww",
+    "topic_alg8_drobi_video": "https://disk.yandex.ru/i/VEOxckIGqysMCQ",
+    "topic_alg8_drobiud_text": "https://disk.yandex.ru/i/eCpwV_vI2sJiag",
+    "topic_alg8_drobiud_video": "https://disk.yandex.ru/i/ORZjVM3bSB1DFw",
+    "topic_geo8_mnogo_text": "https://disk.yandex.ru/i/2zH6SoSq8HmIMg",
+    "topic_geo8_mnogo_video": "https://disk.yandex.ru/i/FRntWHvzRE9WEw",
+    "topic_geo8_mnogoprk_text": "https://disk.yandex.ru/i/kWZpamr-rpn19g",
+    "topic_geo8_mnogoprk_video": "https://disk.yandex.ru/i/KP7OuS37kRtM-Q"
   };
   
   const material = materials[key];
